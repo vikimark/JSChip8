3abf042b727dff59672c71d6bab161db93db9d71
vikimark <mark.phurit@gmail.com>
2021-12-22 09:12:47 +0700

    add font load

--- main.js
+++ main.js
@@ -1,8 +1,12 @@
+const FILE_PATH = 'roms/Stars.ch8'
+
 import {Chip8} from "./chip8.js";
 import * as fs from 'fs';
 
 let chip8 = new Chip8();
 
-fs.readFile('roms/Stars.ch8', null, (err, data) => {
+fs.readFile(FILE_PATH, null, (err, data) => {
     chip8.loadROM(data);
 })
+
+
